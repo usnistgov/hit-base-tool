1fbf20e6220899bfa88e36bdbb5013ee8f30afe1
Crouzier, Nicolas (Assoc) <nicolas.crouzier@nist.gov>
2021-02-11 16:06:19 -0500

    updated varies in profile viewer

--- hit-base-web/client/app/lib/profile-viewer/profile-viewer.js
+++ hit-base-web/client/app/lib/profile-viewer/profile-viewer.js
@@ -447,7 +447,8 @@
 
         field["path"] = parent.path + "-" + field.position;
         var dt = field.datatype;
-        if (dt === 'varies') {
+        // check for varies
+        if (dt.indexOf('var') !== -1) {
           var dynamicMaps = parent.dynamicMaps && parent.dynamicMaps != null && parent.dynamicMaps[field.position] != null ? parent.dynamicMaps[field.position] : null;
           field.children = [];
           if (dynamicMaps != null) {
@@ -571,7 +572,8 @@
           if (node.type === 'SEGMENT_REF') {
             return getNodeChildren($scope.model.segments[node.ref]);
           } else if (node.type === 'FIELD' || node.type === 'COMPONENT') {
-            return node.datatype && node.datatype !== 'varies' && $scope.model.datatypes ? $scope.model.datatypes[node.datatype].children : node.children;
+        	  return node.datatype && node.datatype.indexOf('var') === -1  && $scope.model.datatypes ? $scope.model.datatypes[node.datatype].children : node.children;
+//        	  return node.datatype && node.datatype !== 'varies' && $scope.model.datatypes ? $scope.model.datatypes[node.datatype].children : node.children;
           } else if (node.type === 'DATATYPE' || node.type == 'SEGMENT' || node.type === 'GROUP') {
             return node.children;
           }
@@ -604,7 +606,7 @@
       var processFieldChildrenConstraints = function (parent, removeCandidates) {
         var children = angular.copy(getNodeChildren(parent));
         angular.forEach(children, function (child) {
-          child.type = parent.datatype === 'varies' ? 'DATATYPE' : 'COMPONENT';
+            child.type = parent.datatype.indexOf('var') !== -1 ? 'DATATYPE' : 'COMPONENT';
           child.path = parent.path + "." + child.position;
           child.nodeParent = parent;
           child.selfConformanceStatements = [];
@@ -635,7 +637,7 @@
       var processComponentChildrenConstraints = function (parent, removeCandidates) {
         var children = angular.copy(getNodeChildren(parent));
         angular.forEach(children, function (child) {
-          child.type = parent.datatype === 'varies' ? 'DATATYPE' : 'SUBCOMPONENT';
+            child.type = parent.datatype.indexOf('var') !== -1 ? 'DATATYPE' : 'SUBCOMPONENT';
           child.path = parent.path + "." + child.position;
           child.nodeParent = parent;
           child.selfConformanceStatements = [];
